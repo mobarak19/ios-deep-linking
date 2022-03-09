916242c8d4e1ff433b1684e93830410b02bfccdc
mobarak <mobarakhossen2012@gmail.com>
2022-03-09 12:45:31 +0600

    initial commit

---
+++ IOS Deep Linking - URL Scheme/IOS Deep Linking - URL Scheme/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IOS Deep Linking - URL Scheme
+//
+//  Created by Mobarak on 3/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
